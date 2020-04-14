e54a711689a5891245def4d62fecba50d1328fcf
sylvioGN <syh.gubitoso@gmail.com>
2020-04-15 00:35:34 +0200

    Challenge AceleraDev Data Science

---
+++ main.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Desafio 1
+# 
+# Para esse desafio, vamos trabalhar com o data set [Black Friday](https://www.kaggle.com/mehdidag/black-friday), que reúne dados sobre transações de compras em uma loja de varejo.
+# 
+# Vamos utilizá-lo para praticar a exploração de data sets utilizando pandas. Você pode fazer toda análise neste mesmo notebook, mas as resposta devem estar nos locais indicados.
+# 
+# > Obs.: Por favor, não modifique o nome das funções de resposta.
+
+# ## _Set up_ da análise
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[15]:
+
+
+black_friday = pd.read_csv("black_friday.csv")
+
+
+# ## Inicie sua análise a partir daqui
+
+# In[18]:
+
+
+print(black_friday.head())
+
+
+# ## Questão 1
+# 
+# Quantas observações e quantas colunas há no dataset? Responda no formato de uma tuple `(n_observacoes, n_colunas)`.
+
+# In[4]:
+
+
+def q1():
+    return black_friday.shape
+    pass
+
+
+# ## Questão 2
+# 
+# Há quantas mulheres com idade entre 26 e 35 anos no dataset? Responda como um único escalar.
+
+# In[5]:
+
+
+def q2():
+    F_age = black_friday.groupby(['Gender','Age'])['User_ID'].count()
+    return int(F_age['F']['26-35'])
+    pass
+
+
+# ## Questão 3
+# 
+# Quantos usuários únicos há no dataset? Responda como um único escalar.
+
+# In[6]:
+
+
+def q3():
+    return len(black_friday['User_ID'].unique())
+    pass
+
+
+# ## Questão 4
+# 
+# Quantos tipos de dados diferentes existem no dataset? Responda como um único escalar.
+
+# In[7]:
+
+
+def q4():
+    return len(black_friday.dtypes.value_counts().index)
+    pass
+
+
+# ## Questão 5
+# 
+# Qual porcentagem dos registros possui ao menos um valor null (`None`, `ǸaN` etc)? Responda como um único escalar entre 0 e 1.
+
+# In[8]:
+
+
+def q5():
+    count_NaN = len(black_friday)-len(black_friday.dropna())
+    total_count = black_friday.shape[0]
+    return count_NaN/total_count
+    pass
+
+
+# ## Questão 6
+# 
+# Quantos valores null existem na variável (coluna) com o maior número de null? Responda como um único escalar.
+
+# In[9]:
+
+
+def q6():
+    return int(black_friday.isnull().sum().max())
+    pass
+
+
+# ## Questão 7
+# 
+# Qual o valor mais frequente (sem contar nulls) em `Product_Category_3`? Responda como um único escalar.
+
+# In[10]:
+
+
+def q7():
+    return int(black_friday['Product_Category_3'].mode())
+    pass
+
+
+# ## Questão 8
+# 
+# Qual a nova média da variável (coluna) `Purchase` após sua normalização? Responda como um único escalar.
+
+# In[11]:
+
+
+def q8():
+    Purchase_norm = (black_friday['Purchase'] - black_friday['Purchase'].min())/(black_friday['Purchase'].max() - black_friday['Purchase'].min())
+    return float(Purchase_norm.mean())
+    pass
+
+
+# ## Questão 9
+# 
+# Quantas ocorrências entre -1 e 1 inclusive existem da variáel `Purchase` após sua padronização? Responda como um único escalar.
+
+# In[12]:
+
+
+def q9():
+    purchase_pad = (black_friday['Purchase'] - black_friday['Purchase'].mean())/black_friday['Purchase'].std()
+    return int(purchase_pad[(purchase_pad>-1) & (purchase_pad<=1)].shape[0])
+    pass
+
+
+# ## Questão 10
+# 
+# Podemos afirmar que se uma observação é null em `Product_Category_2` ela também o é em `Product_Category_3`? Responda com um bool (`True`, `False`).
+
+# In[14]:
+
+
+def q10():
+    pc2=black_friday[black_friday['Product_Category_2'].isna()]
+    pc3=pc2[pc2['Product_Category_3'].isna()]
+    return pc2.shape==pc3.shape
+    pass
+
+
+# In[ ]:
+
+
+
+
